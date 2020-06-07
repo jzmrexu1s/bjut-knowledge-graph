8f62688434322ab3e2714441e8f5abf0a9daec96
Chengwei Zhang <1535909927@qq.com>
2020-06-08 00:02:34 +0800

    A new method.

--- task2/getFiltered.py
+++ task2/getFiltered.py
@@ -19,6 +19,51 @@ def checkNotIn(words, sentence):
     return True
 
 
+def separate(rule_pre, rule_type, rule_assert, rule_from, rule_connect, s, d):
+    sp_pre = s.find(rule_pre[0])
+    sp_type = s.find(rule_type[0])
+    sp_assert = None
+    sp_assert_origin = None
+    for rule in rule_assert:
+        if s.find(rule) != -1:
+            sp_assert = s.find(rule)
+            sp_assert_origin = rule
+            break
+    sp_from = None
+    sp_connect = None
+    sp_connect_origin = None
+    if not sp_assert:
+        sp_from = s.find(rule_from[0])
+        for rule in rule_connect:
+            if s.find(rule) != -1:
+                sp_connect = s.find(rule)
+                sp_connect_origin = rule
+                break
+    finance_company = s[:sp_pre]
+    public_company = s[sp_pre + len(rule_pre[0]): sp_type]
+    operation = None
+    if sp_connect:
+        operation = sp_connect_origin[:-1]
+    elif sp_assert:
+        if '定为' in sp_assert_origin:
+            operation = '定为'
+        if '首次定为' in sp_assert_origin:
+            operation = '首次定为'
+        if '维持' in sp_assert_origin:
+            operation = '维持在'
+    if sp_assert or (sp_connect and sp_from):
+        before_val = s[sp_assert + len(sp_assert_origin):] if sp_assert else s[sp_from + len(rule_from[0]): sp_connect]
+        after_val = s[sp_assert + len(sp_assert_origin):] if sp_assert else s[sp_connect + len(sp_connect_origin):]
+        date = str(d)
+        return {'Finance company': finance_company,
+                'Public company': public_company,
+                'Before': before_val,
+                'Operation': operation,
+                'After': after_val,
+                'Date': date}
+    return {}
+
+
 def separate_rate(s, d):
     sp0 = s.find('将')
     sp1 = s.find('评级')
@@ -28,6 +73,7 @@ def separate_rate(s, d):
     sp5 = s.find('下调至')
     sp6 = s.find('首次评级定为')
     sp7 = s.find('初始评级定为')
+    sp8 = s.find('评级定为')
     fc = s[0:sp0]
     pc = ''
     if sp6 != -1:
@@ -43,6 +89,7 @@ def separate_rate(s, d):
         br = s[sp2 + 3:]
         ar = br
         op = '维持'
+
     elif sp4 != -1:
         br = s[sp3 + 1: sp4]
         ar = s[sp4 + 3:]
@@ -51,11 +98,15 @@ def separate_rate(s, d):
         br = s[sp3 + 1: sp5]
         ar = s[sp5 + 3:]
         op = '下调'
+
     elif sp6 != -1:
         ar = s[sp6 + 6:]
         op = '评级'
     elif sp7 != -1:
         ar = s[sp7 + 6:]
+        op = '评级'
+    elif sp8 != -1:
+        ar = s[sp8 + 4:]
         op = '评级'
 
     return {'Finance company': fc,
@@ -75,6 +126,7 @@ def separate_price(s, d):
     sp5 = s.find('下调至')
     sp6 = s.find('首次目标价定为')
     sp7 = s.find('目标价定为')
+    sp8 = s.find('目标价位')
     fc = s[0:sp0]
     pc = ''
     if sp6 != -1:
@@ -90,6 +142,7 @@ def separate_price(s, d):
         br = s[sp2 + 3:]
         ar = br
         op = '维持'
+
     elif sp4 != -1:
         br = s[sp3 + 1: sp4]
         ar = s[sp4 + 3:]
@@ -98,13 +151,21 @@ def separate_price(s, d):
         br = s[sp3 + 1: sp5]
         ar = s[sp5 + 3:]
         op = '下调'
+
     elif sp6 != -1:
         ar = s[sp6 + 6:]
         op = '定为'
     elif sp7 != -1:
         ar = s[sp7 + 6:]
         op = '定为'
-
+    print(
+        {'Finance company': fc,
+         'Public company': pc,
+         'Before price': br,
+         'Operation': op,
+         'After price': ar,
+         'Date': str(d)}
+    )
     return {'Finance company': fc,
             'Public company': pc,
             'Before price': br,
@@ -115,10 +176,11 @@ def separate_price(s, d):
 
 if __name__ == "__main__":
     df = pd.read_csv(FROM_POS)
-    df_rate = pd.DataFrame(columns=('Finance company', 'Public company', 'Before rate', 'Operation', 'After rate', 'Date'))
-    df_price = pd.DataFrame(columns=('Finance company', 'Public company', 'Before price', 'Operation', 'After price', 'Date'))
+    df_rate = pd.DataFrame(columns=('Finance company', 'Public company', 'Before', 'Operation', 'After', 'Date'))
+    df_price = pd.DataFrame(columns=('Finance company', 'Public company', 'Before', 'Operation', 'After', 'Date'))
+    rule_connect = ['上升至', '上升到', '上调至', '上调到', '下调至', '下调到', '上调为', '下调为']
 
-    for i in range(len(df)):
+    for i in range(30000, 35000):
     # for i in range(1000):
         ts = df.iloc[i, 1]
         s = ts
@@ -129,9 +191,11 @@ if __name__ == "__main__":
         if p1 != -1:
             s = s[:p1]
         if checkIn(["评级", "将"], s) and checkNotIn(["目标价"], s):
-            df_rate = df_rate.append([separate_rate(s, df.iloc[i, 2])], ignore_index=True)
+            df_rate = df_rate.append(separate(['将'], ['评级'], ['首次评级定为', '初始评级定为', '评级定为', '维持在'], ['从'],
+                                              rule_connect, s, df.iloc[i, 2]), ignore_index=True)
         elif checkIn(["目标价", "将"], s) and checkNotIn(["评级"], s):
-            df_price = df_price.append([separate_price(s, df.iloc[i, 2])], ignore_index=True)
+            df_price = df_price.append(separate(['将'], ['目标价'], ['首次目标价定为', '初始目标价定为', '目标价定为', '维持在'], ['从'],
+                                              rule_connect, s, df.iloc[i, 2]), ignore_index=True)
 
     df_rate.to_csv("output/rate_separated.csv")
     df_price.to_csv("output/price_separated.csv")
